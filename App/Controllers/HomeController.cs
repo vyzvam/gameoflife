596be38f5c3b191dd9e5072c473cc632962e5940
Subathiran Subramaniam <suba@vyzvam.com>
2015-10-01 21:22:59 +0800

    Do:  - Planning  - Read about Conway's game of life  - Use .Net MVC & Jquery / javascript  - ajax call to make and get requests  - Create model classes, possibly (life, grid, cell)  - Create Handlers for the models  - Create rule class  - processing data in parallel  - include jquery  - javascript, create literal to contain settings, make ajax calls and draw canvas

---
+++ App/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+
+    //TODO: Planning
+    //TODO: Read about Conway's game of life
+    //TODO: Use .Net MVC & Jquery / javascript
+    //TODO: ajax call to make and get requests
+    //TODO: Create model classes, possibly (life, grid, cell)
+    //TODO: Create Handlers for the models
+    //TODO: Create rule class
+    //TODO: processing data in parallel
+    //TODO: include jquery
+    //TODO: javascript, create literal to contain settings, make ajax calls and draw canvas
+
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
