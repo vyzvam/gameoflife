ead28146643610865b3057bdc267a0a215fecfe1
Subathiran Subramaniam <suba@vyzvam.com>
2015-10-04 11:31:56 +0800

    Done:  - Added view file  - planned the home controller actions
    Do:
     - javascript

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -7,25 +7,25 @@ using System.Web.Mvc;
 namespace App.Controllers
 {
 
-    //TODO: Planning
-    //TODO: Read about Conway's game of life
-    //TODO: Use .Net MVC & Jquery / javascript
-    //TODO: ajax call to make and get requests
-    //TODO: Create model classes, possibly (life, grid, cell)
-    //TODO: Create Handlers for the models
-    //TODO: Create rule class
     //TODO: processing data in parallel
     //TODO: include jquery
     //TODO: javascript, create literal to contain settings, make ajax calls and draw canvas
 
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetGridSettings()
+        {
+
+        }
+
+
+        public JsonResult GetGenerationData(string cells)
+        {
         }
 
     }
